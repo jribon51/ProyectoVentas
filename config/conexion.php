89fd937ed92e9d0be19ee51599bde1b7471c8040
jribon51 <josefed89@gmail.com>
2023-11-19 15:52:47 -0500

    19/11/2023 subir archivo de conexion con Db

---
+++ config/conexion.php
@@ -0,0 +1,19 @@
+<?php
+    class Conectar{
+        protected $dbh;
+
+        protected function Conexion(){
+            try {
+                $conectar=$this->dbh=new PDO("sqlsrv:server=localhost;database=CompraVenta","jose","Jribon51");
+                return $conectar;
+            } catch (Exception $e) {
+                print "Error conexion BD". $e->getmessage() ."<br/>";
+                die();
+            }
+        }
+
+
+
+
+    }
+?>
